60ef3ccca8fc6ef2da4c56b2ba0b07693a8c5a58
NIrogen <prirporitien@gmail.com>
2025-11-03 01:05:51 +0300

    add: tg bot php

--- handler.php
+++ handler.php
@@ -1,5 +1,18 @@
 <?php
-$user_name = $_POST["username"];
-$user_phone = $_POST["userphone"];
+$user_name = htmlspecialchars($_POST["username"]);
+$user_phone = htmlspecialchars($_POST["userphone"]);
 
+$token = "8595919228:AAF1CDog6JUrnFCMWkIWOpzbGUUlnHfX9cg";
+$chat_id = "-5002922365";
 
+$formData = array(
+  "Клиент: " => $user_name,
+  "Телефон: " => $user_phone
+);
+
+$text = "";
+foreach($formData as $key => $value) {
+  $text .= $key . "<b>" . urldecode($value) . "</b>" . "%0A" ;
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
