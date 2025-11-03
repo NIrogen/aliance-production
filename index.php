e260ce5616b903f224d39b2fc81d9a670c887cdf
NIrogen <prirporitien@gmail.com>
2025-11-03 20:20:44 +0300

    fix: smt

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<?php
-// ОТКЛЮЧАЕМ CSP
-header_remove("Content-Security-Policy");
-?>
+
 <!DOCTYPE html>
 <html lang="ru">
 
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-eval' https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;">
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
